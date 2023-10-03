0f86f43d84623942e815ea9008846811ef31f35c
R3ndrex <yacholohchtoli@gmail.com>
2023-10-03 11:10:16 +0300

    deleted main

--- interface.py
+++ interface.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 from notifications import notify
 
+duration = 30
+remaining_time = duration
+timer_running = False
+timer_id = [None]
+
 def update_timer():
     global remaining_time, timer_running
     if remaining_time >= 0 and timer_running:
@@ -31,5 +36,29 @@ def stop_timer():
     timer_running = False
     if timer_id[0]:
         app.after_cancel(timer_id[0])
+
+app = tk.Tk()
+app.title("Timer App")
+app.geometry("800x600")
+
+time_display = tk.Label(app, text="Time Left: 00:00:00", font=("Helvetica", 24))
+time_display.pack()
+
+canvas = tk.Canvas(app, width=200, height=200)
+canvas.pack()
+
+progress_bar = ttk.Progressbar(app, orient="horizontal", length=500, mode="determinate", maximum=duration)
+progress_bar.pack()
+
+start_button = tk.Button(app,cursor="hand2", text="Start Timer", command=start_timer)
+start_button.pack()
+
+stop_button = tk.Button(app,cursor="hand2", text="Stop Timer", command=stop_timer)
+stop_button.pack()
+
+custom_label = tk.Label(app, cursor="xterm",text="Custom Time (mm:ss):")
+custom_label.pack()
+
+app.mainloop()
 
 
